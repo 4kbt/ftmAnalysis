832b4899bd89abf4d618b4a8ffc1ea75cbd265d9
Charlie Hagedorn <charlie@hagedorn.xyz>
2020-04-06 22:04:26 -0700

    Adds ods-parsing code. Glacial, but it works.

--- ftmColR.m
+++ ftmColR.m
@@ -1,3 +1,12 @@
 pkg load io
 
+%Straight out of xlsread help docs.
+%ODS import is glacial.
 [numarr txtarr rawarr] = xlsread('data.ods');
+
+
+%Preformat column r cellarray
+colr = [rawarr(:,1) rawarr(:,4) rawarr(:,18)];
+
+%Export.
+save 'columnR.dat' colr
